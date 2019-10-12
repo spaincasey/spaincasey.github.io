0b70f09f1afc808d426b980bdb934e0c6fca3cd6
Casey Spain <spaincasey7@gmail.com>
2019-10-11 18:47:42 -0600

    c

--- Student_Directory/studentDirectory.php
+++ Student_Directory/studentDirectory.php
@@ -1,4 +1,8 @@
 <?php
+    app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
 	$myfile = fopen("studentDirectory.php", "w") or die("Unable to open file!");
 	fwrite($myfile, $_GET["obj"]);
 	fclose($myfile);
